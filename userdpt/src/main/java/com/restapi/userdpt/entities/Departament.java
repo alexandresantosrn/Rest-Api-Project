4c869bfd91366bf39887f8c109a7d772f8b47650
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2024-01-27 22:26:36 -0300

    Criando classes de domínio.

---
+++ userdpt/src/main/java/com/restapi/userdpt/entities/Departament.java
@@ -0,0 +1,28 @@
+package com.restapi.userdpt.entities;
+
+public class Departament {
+
+	private Long id;
+	private String name;
+
+	public Departament() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ userdpt/src/main/java/com/restapi/userdpt/entities/User.java
@@ -0,0 +1,46 @@
+package com.restapi.userdpt.entities;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;
+	private Departament department;
+
+	public User() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Departament getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Departament department) {
+		this.department = department;
+	}
+
+}
