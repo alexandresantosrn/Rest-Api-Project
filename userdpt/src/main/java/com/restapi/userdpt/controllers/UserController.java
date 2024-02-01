3d362647ecbdc9a485d311177e9b2d0eebc12795
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2024-02-01 09:37:52 -0300

    Adicionando description dos métodos.

--- userdpt/src/main/java/com/restapi/userdpt/controllers/UserController.java
+++ userdpt/src/main/java/com/restapi/userdpt/controllers/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.restapi.userdpt.entities.User;
 import com.restapi.userdpt.repositories.UserRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
@@ -20,18 +22,21 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Operation(description = "Busca todos os usuários.")
 	@GetMapping
 	public List<User> findAll() {
 		List<User> result = userRepository.findAll();
 		return result;
 	}
-
+	
+	@Operation(description = "Busca o usuário através do seu id.")
 	@GetMapping(value = "/{id}")
 	public User findById(@PathVariable Long id) {
 		User result = userRepository.findById(id).get();
 		return result;
 	}
-
+	
+	@Operation(description = "Salva um novo usuário na base de dados.")
 	@PostMapping
 	public User insert(@RequestBody User user) {
 		User result = userRepository.save(user);
